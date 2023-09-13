2f1fbd2964178fbc2ced4e4a5738f8cab991df28
Rafaelzg <claudenirorafaelbjj@gmail.com>
2023-09-13 16:40:16 -0300

    fix methods in 'conta.rb'

--- conta.rb
+++ conta.rb
@@ -1,99 +1,59 @@
 require_relative 'cliente'
-#* Programa que mostre as opções de mostrar o saldo, titular e número da conta.
 
 class Conta
-    attr_reader :numero, :titular
-    attr_accessor :saldo
+  attr_reader :numero, :titular
+  attr_accessor :saldo
 
-    def initialize(numero, titular, saldo)
-        @numero = numero
-        @titular = titular
-        @saldo = saldo
-    end
-
-    def depositar(valor)
-        self.saldo += valor
-    end
-
-    def sacar(valor)
-        if saldo >= valor
-            self.saldo -= valor
-            true
-        else
-            puts('Não foi possivel executar o saque')
-        end
-    end
-
-    def transferir(conta_destino, valor)
-        if sacar(valor)
-        conta_destino.depositar(valor)
-        else
-            puts('Não foi possivel executar a transferencia')
-        end
-    end 
-end  
-
-conta = Conta.new(1, 'Rafael Silva', 1500)
-
-def criar_conta()
-
-  def mostrar_saldo_outra_conta(outra_conta)
-    puts "Saldo da conta: #{outra_conta.saldo}"
-  end
-  
-  def mostrar_titular_outra_conta(outra_conta)
-    puts "Titular da conta: #{outra_conta.titular}"
-  end
-  
-  def mostrar_numero_outra_conta(outra_conta)
-    puts "Número da conta: #{outra_conta.numero}"
+  def initialize(numero, titular, saldo)
+    @numero = numero
+    @titular = titular
+    @saldo = saldo
   end
 
+  def depositar(valor)
+    self.saldo += valor
+  end
 
-  outra_conta = Conta.new(2, nome_conta, 1600)
-
-
-  loop do
-    puts '---------------------Criação de Conta---------------------'
-    puts
-    puts
-    puts '-------Escolha uma das seguintes opções-------'
-    puts '1 - Criar Nome'
-    puts '2 - Criar Número '
-    puts '3 - Saldo Inicial'
-    puts '4 - Criar outra conta'
-    puts '0 - Sair'
-
-    result = gets.to_i
-
-    case result
-    when 1
-      system "clear"
-      puts('-------INFO-------')
-      nome_conta = gets.to_s
-    when 2
-      system "clear"
-      puts('-------INFO-------')
-      1.times do
-          puts('-')
-      end
-      mostrar_titular_outra_conta(outra_conta)
-          puts('-')
-    when 3
-      system "clear"
-      puts('-------INFO-------')
-      2.times do
-          puts('-')
-      end
-      mostrar_numero_outra_conta(outra_conta)
-    when 0
-      break
+  def sacar(valor)
+    if saldo >= valor
+      self.saldo -= valor
+      true
     else
-      puts 'Opção inválida. Tente novamente.'
+      false
     end
-
   end
 
+  def transferir(conta_destino, valor)
+    if sacar(valor)
+      conta_destino.depositar(valor)
+      true
+    else
+      false
+    end
+  end
+end
+
+def criar_conta
+  system 'clear'
+  puts '
+  ____ ____  _____    _  _____ _____ 
+ / ___|  _ \| ____|  / \|_   _| ____|
+| |   | |_) |  _|   / _ \ | | |  _|  
+| |___|  _ <| |___ / ___ \| | | |___ 
+ \____|_| \_\_____/_/   \_\_| |_____|
+                                   
+ '
+  puts
+  puts 'Digite o nome: '
+  nome_conta = gets.chomp
+
+  puts 'Digite o número: '
+  numero_conta = gets.to_i
+
+  puts 'Insira o saldo inicial: '
+  saldo_conta = gets.to_i
+
+  return Conta.new(numero_conta, nome_conta, saldo_conta)
 end
 
 def mostrar_saldo(conta)
@@ -108,50 +68,62 @@ def mostrar_numero_conta(conta)
   puts "Número da conta: #{conta.numero}"
 end
 
-loop do
-  puts '-------Escolha uma das seguintes opções-------'
-  puts '1 - Ver Saldo'
-  puts '2 - Ver Titular'
-  puts '3 - Ver Número'
-  puts '4 - Criar outra conta'
-  puts '0 - Sair'
+def tela_inicial
+  conta = nil
 
-  result = gets.to_i
+  loop do
+    system 'clear'
+    puts '
+    _    _ _____ _     _____ ________  ___ _____ 
+   | |  | |  ___| |   /  __ \  _  |  \/  ||  ___|
+   | |  | | |__ | |   | /  \/ | | | .  . || |__  
+   | |/\| |  __|| |   | |   | | | | |\/| ||  __| 
+   \  /\  / |___| |___| \__/\ \_/ / |  | || |___ 
+    \/  \/\____/\_____/\____/\___/\_|  |_/\____/ 
+                                                 
+                                                 
+   '
+    puts '-------Escolha uma das seguintes opções-------'
+    puts '1 - Ver Contas'
+    puts '2 - Ver Titular'
+    puts '3 - Ver Número'
+    puts '4 - Criar outra conta'
+    puts '0 - Sair'
 
-  case result
-  when 1
-    system "clear"
-    puts('-------INFO-------')
-    mostrar_saldo(conta)
-    2.times do
-        puts('-')
-    end
+    result = gets.to_i
 
-  when 2
-    system "clear"
-    puts('-------INFO-------')
-    1.times do
-        puts('-')
+    case result
+    when 1
+      system 'clear'
+      if conta.nil?
+        puts('Conta não criada. Crie uma conta primeiro.')
+      else
+        mostrar_saldo(conta)
+      end
+    when 2
+      system 'clear'
+      if conta.nil?
+        puts('Conta não criada. Crie uma conta primeiro.')
+      else
+        mostrar_titular(conta)
+      end
+    when 3
+      system 'clear'
+      if conta.nil?
+        puts('Conta não criada. Crie uma conta primeiro.')
+      else
+        mostrar_numero_conta(conta)
+      end
+    when 4
+      system 'clear'
+      conta = criar_conta
+    when 0
+      break
+    else
+      puts 'Opção inválida. Tente novamente.'
     end
-    mostrar_titular(conta)
-        puts('-')
-  when 3
-    system "clear"
-    puts('-------INFO-------')
-    2.times do
-        puts('-')
-    end
-    mostrar_numero_conta(conta)
-  when 4
-    
-    puts('conta criada')
-    system "clear"
-    criar_conta()
-    
-    break
-  when 0
-    break
-  else
-    puts 'Opção inválida. Tente novamente.'
   end
 end
+
+tela_inicial
+#Nesta versão revisada, o código lida melhor com a exibição das informações da cont
