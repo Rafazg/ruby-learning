0c5f3ac83d1a2ca8910608b593eef79f6a910b27
Rafael Silva <nightmarezlolzin@gmail.com>
2023-10-02 17:52:22 -0300

    bhaskara excercise

---
+++ structure-conditions/ex002.rb
@@ -0,0 +1,23 @@
+puts ('Digite o coeficiente a: ')
+a = gets.to_f
+
+puts ('Digite o coeficiente b: ')
+b = gets.to_f
+
+puts ('Digite o coeficiente c: ')
+c = gets.to_f
+
+
+
+def deltaFind(a, b, c)
+    delta = b ** 2 - 4 * a * c
+    return delta
+end
+
+def bhaskara(a, b, c)
+    x1 = (-b + Math.sqrt(deltaFind(a, b, c))) / (2 * a)
+    x2 = (-b - Math.sqrt(deltaFind(a, b, c))) / (2 * a)
+    return puts ("X1 = #{x1}, X2 = #{x2}")
+end
+
+bhaskara(a, b, c)
