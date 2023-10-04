564d0052cee7e222b5d8e08917e30cde90fdba63
Rafael Silva <nightmarezlolzin@gmail.com>
2023-10-04 20:43:59 -0300

    exercise to exchange money

---
+++ structure-conditions/ex004.rb
@@ -0,0 +1,15 @@
+
+
+def operadora()
+    puts('Escreva a quantidade de minutos: ')
+    minutos = gets.to_i
+
+    valorPago = 50.0
+    
+    if minutos > 100
+        valorPago = valorPago + 2 * (minutos - 100)
+    end
+    return valorPago
+end
+
+puts("O valor a pagar é de : R$ #{operadora}")
---
+++ structure-conditions/ex005.rb
@@ -0,0 +1,17 @@
+preco_produto = 8.00
+
+puts " Valor do Produto: #{preco_produto}"
+
+puts('Quantos produtos você vai comprar?: ')
+quantidade_comprada = gets.to_i
+
+puts('Insira o valor a pagar: ')
+dinheiro_recebido = gets.to_f
+
+if dinheiro_recebido > preco_produto
+    preco_produto = preco_produto * quantidade_comprada
+    troco = dinheiro_recebido - preco_produto
+    puts("Troco: #{troco}")
+elsif dinheiro_recebido < preco_produto
+    puts('dinheiro insuficiente')
+end
